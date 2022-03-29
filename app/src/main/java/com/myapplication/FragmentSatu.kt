1af22f7d3b390660d8264581aff4d18ed01ce23f
lismanfebryhartin <lismanfh00@gmail.com>
2022-03-29 21:54:56 +0700

    first commit

---
+++ app/src/main/java/com/myapplication/FragmentSatu.kt
@@ -0,0 +1,45 @@
+package com.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class FragmentSatu : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_satu, container, false)
+    }
+
+    override fun onViewCreated(view: View,
+                               savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnDua:Button = view.findViewById(R.id.btn_dua)
+
+        val fragmentDua = FragmentDua()
+        btnDua.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(
+                    R.id.frame_container,
+                    fragmentDua, FragmentDua::class.java.simpleName
+                )
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+        val bundle = Bundle()
+        bundle.putString(FragmentDua.EXTRA_NAME, "Ini data dari fragment satu")
+        val desc = "Fragment ini akan menerima data dari fragment satu"
+
+        fragmentDua.arguments = bundle
+        fragmentDua.desc = desc
+
+
+    }
+}
